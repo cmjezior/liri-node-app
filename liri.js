e14ab50e4df6e04d00a029923a73a793c5dcaa26
Chris Jezior <chrisjezior@Chriss-MacBook-Pro.local>
2018-02-07 17:49:49 -0500

    added spotify keys

--- liri.js
+++ liri.js
@@ -3,3 +3,30 @@ require("dotenv").config();
 var keyfile = require('key.js');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var inputString = process.argv;
+
+var operand = inputString[2];
+var inputContent = inputString[3];
+
+var outputContent;
+
+if (operand === "my-tweets") {
+  outputContent =
+}
+
+else if (operand === "spotify-this-song") {
+  outputContent =
+}
+
+else if (operand === "movie-this") {
+  outputContent =
+}
+
+else if (operand === "do-what-it-says") {
+  outputContent =
+}
+
+else {
+  outputNum = "Not a recognized command";
+}
