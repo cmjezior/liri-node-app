e1276ec8f95a8f506a51ccc6e78df3e83d4af8d2
Chris Jezior <chrisjezior@Chriss-MacBook-Pro.local>
2018-02-28 17:26:58 -0500

    liri change of require keys syntax

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-var keys = require('./keys.js');
+var keys = require('keys.js');
 var action = process.argv[2];
 var value = process.argv[3];
 var request = require('request');
